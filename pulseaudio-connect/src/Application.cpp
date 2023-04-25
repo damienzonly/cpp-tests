f6ccef9a96a468bf06eeeb1202451534c3807ab4
damienzonly <damianomails@gmail.com>
2023-04-26 01:13:20 +0200

    removed mutex logic, embracing the async

--- pulseaudio-connect/src/Application.cpp
+++ pulseaudio-connect/src/Application.cpp
@@ -3,30 +3,32 @@
 #include <chrono>
 #include <thread>
 #include <stdio.h>
-#include <condition_variable>
-#include <mutex>
-
-struct AsyncData {
-    std::mutex mtx;
-    std::condition_variable cv;
-    bool isFinished = false;
-    Application* application;
-};
-
-AsyncData d;
-
-void printThreadId(std::string prefix) {
-    std::cout << prefix << " thread id is ";
-    printf("%x", std::this_thread::get_id());
-    std::cout << std::endl;
-}
 
 void sleep() {
     std::this_thread::sleep_for(std::chrono::seconds(1));
 }
 
-void Application::state_callback(pa_context *c, void *userdata) {
-    auto application = Application::convert_to_application(userdata);
+void quit(const std::string& message) {
+    std::cerr << message << std::endl;
+    exit(1);
+}
+
+void Application::sinkListCallback(pa_context *c, const pa_sink_input_info* info, int eol, void *userdata) {
+    if (!info && eol) return;
+    auto application = Application::convertToApplication(userdata);
+    application->addSinkInput(info);
+}
+
+void Application::clientListCallback(pa_context *c, const pa_client_info* info, int eol, void *userdata) {
+    if (!info && eol) return;
+    if (info != nullptr) {
+        std::cout << "client " << info->name << " index " << info->index << std::endl;
+    }
+}
+
+
+void Application::contextReadyCallback(pa_context *c, void *userdata) {
+    auto application = Application::convertToApplication(userdata);
     auto state = pa_context_get_state(c);
     switch(state) {
         case PA_CONTEXT_CONNECTING:
@@ -34,8 +36,15 @@ void Application::state_callback(pa_context *c, void *userdata) {
             break;
         case PA_CONTEXT_READY: {
             std::cout << "connected to audio server" << std::endl;
-            // printThreadId("state callback");
-            application->start();
+            pa_operation* op;
+            if (!(op = pa_context_get_client_info_list(application->mContext, &Application::clientListCallback, userdata))) {
+                quit("pa_context_get_client_info_list error");
+            }
+            pa_operation_unref(op);
+            if (!(op = pa_context_get_sink_input_info_list(application->mContext, &Application::sinkListCallback, userdata))) {
+                quit("pa_context_get_sink_input_info_list error");
+            }
+            pa_operation_unref(op);
             break;
         }
         case PA_CONTEXT_FAILED:
@@ -47,11 +56,12 @@ void Application::state_callback(pa_context *c, void *userdata) {
     };
 }
 
+
+
 void Application::init() {
-    // printThreadId("init function");
     this->mMainLoop = pa_threaded_mainloop_new();
     this->mContext = pa_context_new(pa_threaded_mainloop_get_api(this->mMainLoop), "myname");
-    pa_context_set_state_callback(mContext, &Application::state_callback, this);
+    pa_context_set_state_callback(mContext, &Application::contextReadyCallback, this);
     pa_context_connect(mContext, nullptr, PA_CONTEXT_NOFLAGS, nullptr);
     pa_threaded_mainloop_start(mMainLoop);
     while (true) {
@@ -59,43 +69,7 @@ void Application::init() {
     }
 }
 
-typedef void (*list_callback_t)(pa_context*, const pa_sink_input_info*, int, void*);
-
-void list_callback(pa_context* c, const pa_sink_input_info* i, int eol, void* userdata) {
-    // printThreadId("list_callback callback");
-    auto asyncData = static_cast<AsyncData*>(userdata);
-    if (i != nullptr) {
-        std::cout << "appending \"" << i->name << "\" to list" << std::endl;
-        asyncData->application->addSinkInput(i);
-    } else if (eol) {
-        std::lock_guard<std::mutex> lock(asyncData->mtx);
-        asyncData->isFinished = true;
-        asyncData->cv.notify_all();
-        return;
-    };
-}
-
 void Application::addSinkInput(const pa_sink_input_info* i) {
-    this->sink_inputs->push_back(i);
-}
-
-void Application::fill_sink_inputs() {
-    d.application = this;
-    d.isFinished = false;
-    pa_operation* op = pa_context_get_sink_input_info_list(this->mContext, list_callback, &d);
-    std::unique_lock<std::mutex> lock(d.mtx);
-    // d.cv.wait(lock, [] { return d.isFinished; });
-    pa_operation_unref(op);
-}
-
-void Application::start() {
-    this->fill_sink_inputs();
-    std::cout << "fill_sink should be finished" << std::endl;
-    for (auto sink: *this->sink_inputs) {
-        std::cout << "sink name: " << sink->name << std::endl;
-    }
-}
-
-void Application::createStreams() {
-    // pa_stream *stream = pa_stream_new(this->mContext, "MyMixer Stream", &sampleSpec, nullptr);
+    std::cout << "index of " << i->name << " is " << i->index << " and client is " << i->client << std::endl;
+    // this->sinkInputs->push_back(i);
 }
--- pulseaudio-connect/src/Application.hpp
+++ pulseaudio-connect/src/Application.hpp
@@ -10,24 +10,25 @@ private:
 public:
     pa_threaded_mainloop* mMainLoop;
     pa_context* mContext;
-    void fill_sink_inputs();
-    std::unique_ptr<sink_inputs_vector> sink_inputs;
+    std::unique_ptr<sink_inputs_vector> sinkInputs;
+
     Application() {
-        this->sink_inputs = std::make_unique<sink_inputs_vector>();
+        this->sinkInputs = std::make_unique<sink_inputs_vector>();
     }
     ~Application() {
         // Free resources
         pa_context_disconnect(this->mContext);
         pa_context_unref(this->mContext);
         pa_threaded_mainloop_free(this->mMainLoop);
-        this->sink_inputs.reset();
+        this->sinkInputs.reset();
     }
+
     void init();
-    void start();
-    static void state_callback(pa_context *c, void *userdata);
-    static Application* convert_to_application(void* userdata) {
+    static void sinkListCallback(pa_context *c, const pa_sink_input_info *i, int eol, void *userdata);
+    static void clientListCallback(pa_context *c, const pa_client_info*i, int eol, void *userdata);
+    static void contextReadyCallback(pa_context *c, void *userdata);
+    static Application* convertToApplication(void* userdata) {
         return static_cast<Application*>(userdata);
     }
     void addSinkInput(const pa_sink_input_info* i);
-    void createStreams();
 };
